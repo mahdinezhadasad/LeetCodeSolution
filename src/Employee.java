fff9190520b9ef763e9e895fd79f8677d5668bc1
mahdinezhadasad <mahdinezhadasad@gmail.com>
2022-11-18 16:04:43 +0100

    Adding some types of sorting

--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Employee {
     
     private String firstName;
@@ -8,6 +10,19 @@ public class Employee {
         this.firstName = firstName;
         this.lastName = lastName;
         this.id = id;
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass () != o.getClass ()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id && firstName.equals (employee.firstName) && lastName.equals (employee.lastName);
+    }
+    
+    @Override
+    public int hashCode() {
+        return Objects.hash (firstName, lastName, id);
     }
     
     public String getFirstName() {
--- src/Employelist.java
+++ src/Employelist.java
@@ -5,11 +5,23 @@ public class Employelist {
     
     public static void main(String[] args) {
         
-        List<Employee> employeeList = new ArrayList<> ();
-        employeeList.add (new Employee ("mahmud","allu",5));
-        employeeList.add (new Employee ("hallowien","eyenigelmis",6));
-        employeeList.add (new Employee ("elle","cokdegil",7));
+//        List<Employee> employeeList = new ArrayList<> ();
+//        employeeList.add (new Employee ("mahmud","allu",5));
+//        employeeList.add (new Employee ("hallowien","eyenigelmis",6));
+//        employeeList.add (new Employee ("elle","cokdegil",7));
+//
+        Employee janeJones  = new Employee ("Jane","Jones",123);
+        Employee johnDoe = new Employee ("John","Doe",457);
+        Employee marySmith = new Employee ("Mary","Smith",22);
+        Employee mikeWilson = new Employee ("Mike","'Wilson",3425);
         
-        employeeList.forEach (employee -> System.out.println (employee));
+        EmployeeLinkedList list = new EmployeeLinkedList ();
+        list.addToFront (janeJones);
+        list.addToFront (johnDoe);
+        list.addToFront (marySmith);
+        list.addToFront (mikeWilson);
+        
+        list.printList ();
+        
     }
 }
